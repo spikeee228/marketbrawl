a46b4ae3ac6fc797e51ea91caf759de74e35004f
spikeee228 <123marat.izm@gmail.com>
2021-12-25 18:21:02 +0200

    baba timura

--- js.js
+++ js.js
@@ -1,54 +1,35 @@
-let elem = document.getElementById('grid')
-let xhr = new XMLHttpRequest
-let url = 'https://my-json-server.typicode.com/spikeee228/marketbrawl'
-xhr.open("GET", url + "/products/")
-xhr.responseType = 'json'
-xhr.send()
+let productsGrid = document.getElementById('products-grid');
+let productsArray = [];
+let xhr = new XMLHttpRequest();
+let url = 'https://my-json-server.typicode.com/RobocodeSchool/marketplace';
 
+xhr.open('GET', url + '/products/');
+xhr.responseType = 'json';
 xhr.onload = function() {
-    let products = xhr.response
-    console.log(products)
-    elem.innerHTML += `
-      <div class = "item">
-         <h1>${products[0].name}</h1>
-         <img width="300px" src='${products[0].photo_url}'>
-         <h1>Description: ${products[0].description}</h1>
-         <h1>Price: ${products[0].price}</h1>
-         </div>
-         <div class = "item">
-         <h1>${products[1].name}</h1>
-         <img width="300px" src='${products[1].photo_url}'>
-         <h1>Description: ${products[1].description}</h1>
-         <h1>Price: ${products[1].price}</h1>
-         </div>
-         <div class = "item">
-         <h1>${products[2].name}</h1>
-         <img width="300px" src='${products[2].photo_url}'>
-         <h1>Description: ${products[2].description}</h1>
-         <h1>Price: ${products[2].price}</h1>
-         </div>
-         <div class = "item">
-         <h1>${products[3].name}</h1>
-         <img width="300px" src='${products[3].photo_url}'>
-         <h1>Description: ${products[3].description}</h1>
-         <h1>Price: ${products[3].price}</h1>
-         </div>
-         <div class = "item">
-         <h1>${products[4].name}</h1>
-         <img width="300px" src='${products[4].photo_url}'>
-         <h1>Description: ${products[4].description}</h1>
-         <h1>Price: ${products[4].price}</h1>
-         </div>
-         <div class = "item">
-         <h1>${products[5].name}</h1>
-         <img width="300px" src='${products[5].photo_url}'>
-         <h1>Description: ${products[5].description}</h1>
-         <h1>Price: ${products[5].price}</h1>
-         </div>
-       
-         
-         
-    `
-    
-    
+	let products = xhr.response;
+	productsGrid.innerHTML = null;
+
+	products.forEach(p => {
+		productsArray.push(p);
+		let pElem = document.createElement('div');
+		pElem.innerHTML = `
+		<div class="product">
+		<h2 class='product-name'>${p.name}</h2>
+		<img class='product-photo' src='${p.photo_url}'
+		alt='${p.name}'>
+		<p class='product-price'><b>Price: <b>${p.price}$</p>
+		<p class='product-description'><b>Description: </b>${p.description}</p>
+		<a href='index.html?id=${p.author_id}'>Seller profile</a>
+		<button>Buy</button>
+		</button>
+		`;
+		productsGrid.append(pElem);
+		});
 }
+xhr.send();
+
+let cartProducts = document.getElementById('cart-products');
+
+  function openCart(){
+	cartProducts.classList.toggle('hide')
+  }
